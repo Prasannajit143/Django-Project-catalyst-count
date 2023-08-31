fb2646ecccfd311d8a99b0127ba455b326952e93
Prasannajit <prasannajitkumbhare@gmail.com>
2023-08-31 15:38:42 +0530

    User Page Completed

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('home',views.welcome,name="home"),
     path('query_builder',views.query_builder,name="query_builder"),
     path('users',views.users,name="users"),
+    path('delete/<int:pk>/',views.delete,name="delete"),
+    path('add_user/',views.add_user,name="add_user"),
+    path('save_user',views.save_user,name="save_user")
 ]
+
+# fdlkjl
--- app/views.py
+++ app/views.py
@@ -12,3 +12,20 @@ def query_builder(request):
 def users(requst):
     users = User.objects.all()
     return render(requst,'user.html',{'users': users})
+
+def delete(requst,pk):
+    user = User.objects.get(pk=pk)
+    user.delete()
+    return redirect('/users')
+
+def add_user(request):
+    return render(request,'add_user.html')
+
+def save_user(requst):
+    username = requst.POST.get('username')
+    password = requst.POST.get('password')
+    email = requst.POST.get('email')
+    new_user = User(username=username, email=email)
+    new_user.set_password(password)
+    new_user.save()
+    return redirect('/users')
